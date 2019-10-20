f31fdb4312b21989c9e6b2107006bf3603894b41
wangyanchang21 <wangyanchang21@163.com>
2019-10-20 22:33:57 +0800

    Update AppConfig.java
    appsecret expired explain

--- weapp-server/src/main/java/com/config/AppConfig.java
+++ weapp-server/src/main/java/com/config/AppConfig.java
@@ -2,10 +2,11 @@ package com.config;
 
 /**
  * 项目中的常量定义类
+ * appsecret已失效, 请替换为你的appid和appsecret
  */
 public class AppConfig {
     
     public static final String APP_ID = "wx08dda6d91ce57082";
-    
+
     public static final String APP_SECRET="cb62562c2cb8475fdf0908f629154a75";
 }
